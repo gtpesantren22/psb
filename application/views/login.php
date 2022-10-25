fae59b9b95f720ee7d9c1522c20b29930fed8d3e
gtpesantren26 <cakromat26@gmail.com>
2022-10-25 08:28:55 +0700

    Login form

--- application/views/login.php
+++ application/views/login.php
@@ -5,67 +5,253 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login Page - PSB 2023</title>
 
-    <link rel="stylesheet" href="<?= base_url(); ?>assets/modules/izitoast/css/iziToast.min.css">
-    <link href="<?= base_url(); ?>assets/css/front.min.css" rel="stylesheet" />
-    <link rel="shortcut icon" href="<?= base_url(); ?>assets/img/logo/Logo_Pesantren.png">
-    <link rel="stylesheet" href="<?= base_url(); ?>assets/css/1.css">
-    <link rel="stylesheet" href="<?= base_url(); ?>assets/css/2.css">
-    <link rel="stylesheet" href="<?= base_url(); ?>assets/css/3.css">
-    <link rel="stylesheet" href="<?= base_url(); ?>assets/css/components2.css">
-    <script src="<?= base_url(); ?>assets/modules/sweetalert/sweetalert.min.js"></script>
-    <link rel="stylesheet" href="<?= base_url(); ?>assets/modules/font-awesome/css/font-awesome.css">
-    <link rel="stylesheet" type="text/css" href="<?= base_url(); ?>https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+        body {
+            color: #000;
+            overflow-x: hidden;
+            height: 100%;
+            background-color: #B0BEC5;
+            background-repeat: no-repeat;
+        }
+
+        .card0 {
+            box-shadow: 0px 4px 8px 0px #757575;
+            border-radius: 0px;
+        }
+
+        .card2 {
+            margin: 0px 40px;
+        }
+
+        .logo {
+            width: 200px;
+            height: 100px;
+            margin-top: 20px;
+            margin-left: 35px;
+        }
+
+        .image {
+            width: 360px;
+            height: 280px;
+        }
+
+        .border-line {
+            border-right: 1px solid #EEEEEE;
+        }
+
+        .facebook {
+            background-color: #3b5998;
+            color: #fff;
+            font-size: 18px;
+            padding-top: 5px;
+            border-radius: 50%;
+            width: 35px;
+            height: 35px;
+            cursor: pointer;
+        }
+
+        .twitter {
+            background-color: #1DA1F2;
+            color: #fff;
+            font-size: 18px;
+            padding-top: 5px;
+            border-radius: 50%;
+            width: 35px;
+            height: 35px;
+            cursor: pointer;
+        }
+
+        .linkedin {
+            background-color: #2867B2;
+            color: #fff;
+            font-size: 18px;
+            padding-top: 5px;
+            border-radius: 50%;
+            width: 35px;
+            height: 35px;
+            cursor: pointer;
+        }
+
+        .line {
+            height: 1px;
+            width: 45%;
+            background-color: #E0E0E0;
+            margin-top: 10px;
+        }
+
+        .or {
+            width: 10%;
+            font-weight: bold;
+        }
+
+        .text-sm {
+            font-size: 14px !important;
+        }
+
+        ::placeholder {
+            color: #BDBDBD;
+            opacity: 1;
+            font-weight: 300
+        }
+
+        :-ms-input-placeholder {
+            color: #BDBDBD;
+            font-weight: 300
+        }
+
+        ::-ms-input-placeholder {
+            color: #BDBDBD;
+            font-weight: 300
+        }
+
+        input,
+        textarea {
+            padding: 10px 12px 10px 12px;
+            border: 1px solid lightgrey;
+            border-radius: 2px;
+            margin-bottom: 5px;
+            margin-top: 2px;
+            width: 100%;
+            box-sizing: border-box;
+            color: #2C3E50;
+            font-size: 14px;
+            letter-spacing: 1px;
+        }
+
+        input:focus,
+        textarea:focus {
+            -moz-box-shadow: none !important;
+            -webkit-box-shadow: none !important;
+            box-shadow: none !important;
+            border: 1px solid #304FFE;
+            outline-width: 0;
+        }
+
+        button:focus {
+            -moz-box-shadow: none !important;
+            -webkit-box-shadow: none !important;
+            box-shadow: none !important;
+            outline-width: 0;
+        }
+
+        a {
+            color: inherit;
+            cursor: pointer;
+        }
+
+        .btn-blue {
+            background-color: #1A237E;
+            width: 150px;
+            color: #fff;
+            border-radius: 2px;
+        }
+
+        .btn-blue:hover {
+            background-color: #000;
+            cursor: pointer;
+        }
+
+        .bg-blue {
+            color: #fff;
+            background-color: #1A237E;
+        }
+
+        @media screen and (max-width: 991px) {
+            .logo {
+                margin-left: 0px;
+            }
+
+            .image {
+                width: 300px;
+                height: 220px;
+            }
+
+            .border-line {
+                border-right: none;
+            }
+
+            .card2 {
+                border-top: 1px solid #EEEEEE !important;
+                margin: 0px 15px;
+            }
+        }
+    </style>
 </head>
 
 <body>
-    <div class="row">
-        <div class="col-sm-8 d-flex ">
-            <div class="col-md-12 animated bounceInLeft">
-                <div class="card mt-2 mb-2">
-                    <div class="card-header text-white" style="background-color: #005f6b">
-                        <h4>Form Pendaftaran Santri Baru</h4>
-
-                    </div>
-                    <div class="card-body">
-                        <div class="col-12 animated bounceIn">
-                            <div class="activities">
-                                <div class="activity">
-
-                                    <div class="activity-detail">
-                                        <?= form_open('daftar/daftarAct'); ?>
-                                        <div class="row">
-                                            <div class="col-md-12">
-                                                <div class="form-group">
-                                                    <label for="nama">NIK</label>
-                                                    <input type="text" maxlength="16" class="form-control" name="nik" required>
-                                                </div>
-                                                <div class="form-group">
-                                                    <label for="nohp">No. KK</label>
-                                                    <input type="text" maxlength="16" class="form-control" name="no_kk" required>
-                                                </div>
-                                            </div>
-                                            <div class="col-md-1">
-                                                &nbsp;
-                                            </div>
-                                        </div>
-                                        <div class="col-md-8 col-lg-12">
-                                            <button type="submit" id="btnsimpan" class="btn btn-primary btn-block"><i class="fa fa-check-square fa-lg fa-fw"></i> Daftar</button>
-                                        </div>
-                                        <?= form_open(); ?>
-                                    </div>
-
-                                </div>
-
+    <div class="container-fluid px-1 px-md-5 px-lg-1 px-xl-5 py-5 mx-auto">
+        <div class="card card0 border-0">
+            <div class="row d-flex">
+                <div class="col-lg-6">
+                    <div class="card1 pb-5">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <img src="<?= base_url('assets/img/logo/logo-23.png') ?>" class="img-fluid" width="220">
                             </div>
-
                         </div>
+                        <div class="row px-3 justify-content-center mt-4 mb-5 border-line">
+                            <img src="<?= base_url('assets/img/logo/LOGO-PSB-2022-2.png') ?>" class="img-fluid" width="300">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="card2 card border-0 px-4 py-5">
+                        <div class="row mb-4 px-3">
+                            <h6 class="mb-0 mr-4 mt-2">Silahkan login menggunakan akun yang sudah dikirimkan!</h6>
+                        </div>
+                        <div class="row px-3 mb-4">
+                            <div class="line"></div>
+                            <small class="or text-center">Login</small>
+                            <div class="line"></div>
+                        </div>
+                        <div class="row px-3">
+                            <label class="mb-1">
+                                <h6 class="mb-0 text-sm">Username</h6>
+                            </label>
+                            <input class="mb-4" type="text" name="username" placeholder="Masukan User">
+                        </div>
+                        <div class="row px-3">
+                            <label class="mb-1">
+                                <h6 class="mb-0 text-sm">Password</h6>
+                            </label>
+                            <input type="password" name="password" placeholder="Masukan password">
+                        </div>
+                        <div class="row px-3 mb-4">
+                            <div class="custom-control custom-checkbox custom-control-inline">
+                                <input id="chk1" type="checkbox" name="chk" class="custom-control-input">
+                                <label for="chk1" class="custom-control-label text-sm">Remember me</label>
+                            </div>
+                            <a href="#" class="ml-auto mb-0 text-sm">Forgot Password?</a>
+                        </div>
+                        <div class="row mb-3 px-3">
+                            <button type="submit" class="btn btn-blue text-center">Login</button>
+                        </div>
+                        <div class="row mb-4 px-3">
+                            <small class="font-weight-bold">Don't have an account? <a class="text-danger" href="<?= base_url('daftar') ?>">Daftar disni!</a></small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="bg-blue py-4">
+                <div class="row px-3">
+                    <small class="ml-4 ml-sm-5 mb-2">PSB 2023 - PP Darul Lughah Wal Karomah</small>
+                    <div class="social-contact ml-4 ml-sm-auto">
+                        <span class="fa fa-facebook mr-4 text-sm"></span>
+                        <span class="fa fa-instagram mr-4 text-sm"></span>
+                        <span class="fa fa-youtube mr-4 text-sm"></span>
+                        <span class="fa fa-twitter mr-4 mr-sm-5 text-sm"></span>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </body>
+
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
 </html>
